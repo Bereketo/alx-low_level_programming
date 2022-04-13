ae3851c10bba0c52c837353eb0a4614249f8a02d
Bereketo <bereketalebachew87@gmail.com>
2022-04-13 09:58:12 -0400

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+
+* main entry block.
+* @void no argument
+* return 0;
+**/
+
+
+int main(void)
+{ 
+
+   char Bek[]="_putchar";
+   int c;
+   for (c=0; c<8; c++)
+  { _putchar(Bek[c]);
+    }
+  _putchar('\n');
+  return(0);
+} 
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+ 
