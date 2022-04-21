bb43a13bab82da55a2ed2541824e7cafbaf0099c
Bereketo <bereketalebachew87@gmail.com>
2022-04-21 14:49:57 -0400

    my commit message

---
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char s1[98] = "Hello ";
+char s2[] = "World!\n";
+char *ptr;
+
+_putchar("%s\n", s1);
+_putchar("%s", s2);
+ ptr = _strcat(s1, s2);
+_putchar("%s", s1);
+_putchar("%s", s2);
+_putchar("%s", ptr);
+return (0);
+}
+
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is a readme file for the project 0x06-pointers_arrays_strings
