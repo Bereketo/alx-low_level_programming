5b6fb20042d64d74eee2d1eb4712103feaedb12a
Bereketo <bereketalebachew87@gmail.com>
2022-10-03 20:15:57 -0400

    dynamic libraries

---
+++ 0x18-dynamic_libraries/my_functions.c
@@ -0,0 +1,29 @@
+#include "100-main.h"
+/**
+ * takes two integers or floates 
+ * return the sum, differece, product, quotient or remainder of the two numbers
+ */
+int add(int a, int b)
+{ 
+ return (a+b);
+}
+
+int sub(int a, int b)
+{
+ return (a-b);
+}
+
+int mul(int a, int b)
+{
+ return (a*b);
+}
+
+int div(int a, int b)
+{
+ return (a/b);
+}
+
+int mod(int a, int b)
+{
+  return (a%b);
+}
